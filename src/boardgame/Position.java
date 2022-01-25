476a58ba8a631fe0c434c25447c95bdbac5a71bb
Luiz Henrique Silva Sampaio <luizhsampaio07@gmail.com>
2022-01-25 09:56:46 -0300

    Position class added

---
+++ src/boardgame/Position.java
@@ -0,0 +1,21 @@
+package boardgame;
+
+public class Position {
+    private int row;
+    private int column;
+
+    public Position(int row, int column) {
+        this.row = row;
+        this.column = column;
+    }
+
+    public void setPosition(int row, int column) {
+        this.row = row;
+        this.column = column;
+    }
+
+    @Override
+    public String toString() {
+        return row + ", " + column;
+    }
+}
