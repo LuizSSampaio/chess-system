fed15701d9f853e8c3f4dac305cb68b845bf6357
Luiz Henrique Silva Sampaio <luizhsampaio07@gmail.com>
2022-01-26 10:48:02 -0300

    Printing possible moves

--- src/application/Program.java
+++ src/application/Program.java
@@ -25,6 +25,10 @@ public class Program {
                 System.out.print("Source: ");
                 ChessPosition source = UI.readChessPosition(sc);
 
+                boolean[][] possibleMoves = chessMatch.possibleMoves(source);
+                UI.clearScreen();
+                UI.printBoard(chessMatch.getPieces(), possibleMoves);
+
                 System.out.println();
                 System.out.print("Target: ");
                 ChessPosition target = UI.readChessPosition(sc);
@@ -34,6 +38,9 @@ public class Program {
             catch (ChessException | InputMismatchException err) {
                 System.out.println(err.getMessage());
                 sc.nextLine();
+            }
+            finally {
+                UI.clearScreen();
             }
         } while (true);
     }
